a03924e189879b20ecea9f46eaf32511ad6e2140
mandihamza <mandihamza@gmail.com>
2021-11-09 13:00:34 -0700

    init commit

---
+++ pages/404.js
@@ -0,0 +1,3 @@
+export default function Error404() {
+  return <img src="/404-error-cat.svg" alt="404 image" className="err-404-image"/>
+}
---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>mandi.dev</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
+      <main>
+        <Header />
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
