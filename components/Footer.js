ec480b88239067ad524a3337a73cab65df506189
mandihamza <mandihamza@gmail.com>
2021-11-11 14:45:15 -0700

    refactor footer to styled component

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,14 @@
-import styles from './Footer.module.css'
+import styled from 'styled-components'
 
 export default function Footer() {
-  return (
-    <>
-      <footer className={styles.footer}>
-      </footer>
-    </>
-  )
+  return <StyledFooter />
 }
+
+const StyledFooter = styled.footer`
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid #eaeaea;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
