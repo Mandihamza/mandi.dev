7a57df8b3e6fcd0a5488c90780a088e38b1980eb
mandihamza <mandihamza@gmail.com>
2021-11-11 13:30:07 -0700

    refactor 404 page as styled component

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,15 @@
+import styled from 'styled-components'
+
 export default function Error404() {
-  return <img src="/404-error-cat.svg" alt="404 image" className="err-404-image"/>
+  return (
+    <>
+      <Styled404 src="/404-error-cat.svg" alt="404 image" />
+    </>
+  )
 }
+
+const Styled404 = styled.img`
+  margin: 0 auto;
+  width: 100vw;
+  height: 100vh;
+`
